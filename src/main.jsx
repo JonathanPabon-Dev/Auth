5f801bb5e57f5b6e2db1b6dd57cf9bd262512c5d
Jonathan Pabon <jonathanpabon.dev@gmail.com>
2025-03-15 15:28:53 -0500

    Fix: route base name

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ const root = document.getElementById("root");
 
 createRoot(root).render(
   <StrictMode>
-    <BrowserRouter basename="/Auth/">
+    <BrowserRouter basename="/Auth">
       <App />
     </BrowserRouter>
   </StrictMode>,
